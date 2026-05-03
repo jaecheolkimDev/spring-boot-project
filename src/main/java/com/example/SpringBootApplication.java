cf13539b18260471f739ab17fa4ad221bedddfc0
jaecheolKim <jcdoom@naver.com>
2026-05-03 22:48:31 +0900

    Revert "2026-05-03 토큰 정리"
    This reverts commit 99dd82c6745e91c5e4df4e17ebd889af7c50d1ba.

--- src/main/java/com/example/SpringBootApplication.java
+++ src/main/java/com/example/SpringBootApplication.java
@@ -1,11 +1,8 @@
 package com.example;
 
-import com.example.config.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
 public class SpringBootApplication {
 
 	public static void main(String[] args) {
